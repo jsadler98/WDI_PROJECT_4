1298658fae281249aaada59279a61005225ae7a0
jsadler98 <joshy.sadler@btinternet.com>
2018-01-23 14:01:41 +0000

    buttons

--- src/components/utility/Navbar.js
+++ src/components/utility/Navbar.js
@@ -31,17 +31,17 @@ const Navbar = ({ history }) => {
       </div>
       <div className="navbar-collapse collapse">
         <ul className="nav navbar-right">
-          <a className="main-button">
+          <button className="main-button">
             { !Auth.isAuthenticated() &&  <Link to="/login" className="standard-button">Login</Link>}
-          </a>
+          </button>
           {' '}
-          <a className="main-button">
+          <button className="main-button">
             { !Auth.isAuthenticated() && <Link to="/register" className="standard-button">Register</Link>}
-          </a>
+          </button>
           {' '}
-          <a className="main-button">
+          <button className="main-button">
             { Auth.isAuthenticated() && <a href="#" className="standard-button" onClick={logout}>Logout</a>}
-          </a>
+          </button>
         </ul>
       </div>
     </nav>
